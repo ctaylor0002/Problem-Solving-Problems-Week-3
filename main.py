709328cd5cc2bb13cd7ca88953a2e13f53d1e0b6
ctaylor0002 <charles4312@comcast.net>
2022-08-07 15:25:29 -0400

    Solved problem three by utilizing a for loop to look at every letter in the string. Works but has an issue if there is a single character as the last letter.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from problem_solving_one import problem_one, problem_two
+from problem_solving_one import problem_one, problem_three, problem_two
 
 #Reverse a string
 #I will be utilizing a function from problem_solving_one.py
@@ -17,3 +17,11 @@ capitalize_send_data = input("Please input a sentence. ")
 capitalize_return_data = problem_two(capitalize_send_data)
 print(f"{capitalize_send_data} capitalized is, {capitalize_return_data}")
 
+#Compress a string of characters
+#Utilize a for loop and a counter with an if statement checking if the previous index is the same
+#If it is then I will add to the counter until it isnt then we will concat the counter as well as the letter.
+#Reset the counter and repeat. Also removing eacSh letter as we search for it
+
+compress_send_data = "aaabbbbbccccddddeeeedda"
+compress_return_data = problem_three(compress_send_data)
+print(f"{compress_send_data} compressed down is, {compress_return_data}")
--- problem_solving_one.py
+++ problem_solving_one.py
@@ -8,3 +8,31 @@ def problem_two(sentence):
         return_data = return_data + " " + word.title()
 
     return return_data
+
+def problem_three(data):
+    send_data = ""
+    str_counter = 0
+    counter = 0
+    str_len = len(data)
+
+    for character in data:
+        if str_counter == str_len - 1:
+            send_data = send_data + str(counter + 1) + data[str_counter -1]
+            counter = 0
+            str_counter = 0
+        else:
+            if str_counter == 0:
+                counter += 1
+                str_counter += 1
+            elif str_counter == str_len:
+                send_data = send_data + str(counter) + character
+        
+            elif character == data[str_counter-1]:
+                counter += 1
+                str_counter += 1
+            else:
+                send_data = send_data + str(counter) + data[str_counter -1]
+                counter = 1
+                str_counter += 1
+
+    return(send_data)
