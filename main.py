a289cc5a45fd750a1b127870efe25f66a3565a2e
ctaylor0002 <charles4312@comcast.net>
2022-08-07 14:45:40 -0400

    Solved problem 2 by utilizing the split method to break the sentence into a list then a for loop with the title method to capitalize and concat back into a string.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from problem_solving_one import problem_one
+from problem_solving_one import problem_one, problem_two
 
 #Reverse a string
 #I will be utilizing a function from problem_solving_one.py
@@ -9,3 +9,11 @@ from problem_solving_one import problem_one
 reverse_send_data = input("Please input a sentence. ")
 reverse_return_data = problem_one(reverse_send_data)
 print(f"{reverse_send_data} reversed is, {reverse_return_data}")
+
+#Capitalize letter
+#My thought process is to take a sentence, seperate it into a list based on the spaces, then use the title method to capitalize and input into a string.
+
+capitalize_send_data = input("Please input a sentence. ")
+capitalize_return_data = problem_two(capitalize_send_data)
+print(f"{capitalize_send_data} capitalized is, {capitalize_return_data}")
+
--- problem_solving_one.py
+++ problem_solving_one.py
@@ -1,2 +1,10 @@
 def problem_one(sentence):
     return sentence[::-1]
+
+def problem_two(sentence):
+    split_sentence = sentence.split()
+    return_data = ""
+    for word in split_sentence:
+        return_data = return_data + " " + word.title()
+
+    return return_data
